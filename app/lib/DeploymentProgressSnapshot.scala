84e9012416c2f3e793143800d03d277e5e7e67a9
Roberto Tyley <roberto.tyley@gmail.com>
2014-09-05 13:06:17 +0100

    PullRequestSiteCheck focuses down to a specific PRs status

--- app/lib/DeploymentProgressSnapshot.scala
+++ app/lib/DeploymentProgressSnapshot.scala
@@ -1,15 +1,16 @@
 package lib
 
+import java.util.concurrent.TimeUnit
+
 import com.github.nscala_time.time.Imports._
 import com.madgag.git._
 import lib.LabelledState._
 import org.eclipse.jgit.revwalk.{RevCommit, RevWalk}
-import org.joda.time.DateTime
 import org.kohsuke.github.GHPullRequest
 import play.api.Logger
-
-import scala.collection.convert.wrapAsScala._
-
+import play.api.libs.concurrent.Akka
+import play.api.Play.current
+import scala.concurrent.ExecutionContext.Implicits.global
 
 sealed trait PullRequestDeploymentStatus {
   def labelFor(site: Site) = getClass.getSimpleName.dropRight(1) + " on " + site.label
@@ -32,6 +33,9 @@ case object Overdue extends NotSeenOnSite
 
 
 case class DeploymentProgressSnapshot(repoSnapshot: RepoSnapshot, siteSnapshot: SiteSnapshot) {
+  private implicit val system = Akka.system
+
+  private val smallDelayToForceCommentToAppearAfterLabelChanges = concurrent.duration.Duration(1, TimeUnit.SECONDS)
 
   val OverdueThreshold = 15.minutes
 
@@ -42,29 +46,32 @@ case class DeploymentProgressSnapshot(repoSnapshot: RepoSnapshot, siteSnapshot: 
   }
 
   def handlePR(pr : GHPullRequest) {
+
     Logger.trace(s"handling ${pr.getNumber}")
     val issueHack = repoSnapshot.repo.getIssue(pr.getNumber)
     val labelledState = issueHack.labelledState(_ => true)
     val existingState = PullRequestDeploymentStatus.fromLabels(labelledState.applicableLabels, siteSnapshot.site).getOrElse(Pending)
 
     if (existingState != Seen) {
-      val timeSinceMerge = (new DateTime(pr.getMergedAt) to siteSnapshot.time).duration
 
-      val currentState = if (isVisibleOnSite(pr)) Seen else if (timeSinceMerge > OverdueThreshold) Overdue else Pending
+      val prsc = PullRequestSiteCheck(pr, siteSnapshot, repoSnapshot.gitRepo)
 
       // update labels before comments - looks better on pull request page
-      labelledState.updateLabels(Set(currentState.labelFor(siteSnapshot.site)))
+      labelledState.updateLabels(Set(prsc.label))
 
-      if (timeSinceMerge < WorthyOfCommentWindow) {
+      if (prsc.timeSinceMerge < WorthyOfCommentWindow) {
         Logger.info("Totally worthy of comment!")
-        currentState match {
-          case Pending => Logger.info("Still waiting for this one")
-          case Seen =>
-            // Logger.info("This pull request has been seen on-site!")
-            pr.comment(views.html.ghIssues.seen(siteSnapshot.site, timeSinceMerge).body)
-          case Overdue =>
-            Logger.info("Overdue!")
-            // pr.comment(views.html.ghIssues.overdue(siteSnapshot.site, timeSinceMerge).body)
+
+        system.scheduler.scheduleOnce(smallDelayToForceCommentToAppearAfterLabelChanges) {
+          prsc.currentState match {
+            case Pending => Logger.info("Still waiting for this one")
+            case Seen =>
+              // Logger.info("This pull request has been seen on-site!")
+              pr.comment(views.html.ghIssues.seen(prsc).body)
+            case Overdue =>
+              Logger.info("Overdue!")
+              pr.comment(views.html.ghIssues.overdue(prsc).body)
+          }
         }
       }
 
---
+++ app/lib/PullRequestSiteCheck.scala
@@ -0,0 +1,35 @@
+package lib
+
+
+import com.github.nscala_time.time.Imports._
+import com.madgag.git._
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.revwalk.{RevCommit, RevWalk}
+import org.joda.time.DateTime
+import org.kohsuke.github.GHPullRequest
+import play.api.Logger
+
+case class PullRequestSiteCheck(pr: GHPullRequest, siteSnapshot: SiteSnapshot, gitRepo: Repository) {
+
+  val site = siteSnapshot.site
+
+  val OverdueThreshold = 15.minutes
+
+  val timeSinceMerge = (new DateTime(pr.getMergedAt) to siteSnapshot.time).duration
+
+  val isVisibleOnSite: Boolean = {
+    implicit val w: RevWalk = new RevWalk(gitRepo)
+    val prCommit: RevCommit = pr.getHead.getSha.asObjectId.asRevCommit
+    val siteCommit: RevCommit = siteSnapshot.commitId.get.asRevCommit
+
+    val isVisible = w.isMergedInto(prCommit,siteCommit)
+
+    Logger.info(s"prCommit=${prCommit.name()} siteCommit=${siteCommit.name()} isVisible=$isVisible")
+
+    isVisible
+  }
+
+  val currentState = if (isVisibleOnSite) Seen else if (timeSinceMerge > OverdueThreshold) Overdue else Pending
+
+  val label = currentState.labelFor(siteSnapshot.site)
+}
