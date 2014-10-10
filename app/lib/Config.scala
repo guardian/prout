d2b5278d4ce43ca2f173b20a85b1ce3bcccdce55
Roberto Tyley <roberto.tyley@gmail.com>
2014-10-10 18:20:53 +0100

    Read checkpoint config from .prout.json files anywhere in the file tree

---
+++ app/lib/Config.scala
@@ -0,0 +1,58 @@
+package lib
+
+import com.madgag.git._
+import com.netaporter.uri.Uri
+import com.github.nscala_time.time.Implicits._
+import org.eclipse.jgit.lib.{ObjectReader, ObjectId}
+import org.joda.time.Period
+import play.api.data.validation.ValidationError
+import play.api.libs.json._
+import play.api.libs.json.Json
+
+object Config {
+
+  def readsParseableString[T](parser: String => T): Reads[T] = new Reads[T] {
+    def reads(json: JsValue): JsResult[T] = json match {
+      case JsString(s) => parse(s) match {
+        case Some(d) => JsSuccess(d)
+        case None => JsError(Seq(JsPath() -> Seq(ValidationError("Error parsing string"))))
+      }
+      case _ => JsError(Seq(JsPath() -> Seq(ValidationError("Expected string"))))
+    }
+
+    private def parse(input: String): Option[T] =
+    scala.util.control.Exception.allCatch[T] opt (parser(input))
+
+  }
+
+  implicit val readsPeriod: Reads[Period] = readsParseableString(input => Period.parse("PT"+input))
+
+  implicit val readsUri: Reads[Uri] = readsParseableString(input => Uri.parse(input))
+
+  implicit val readsCheckpointDetails: Reads[CheckpointDetails] = Json.reads[CheckpointDetails]
+
+//  implicit val readsCheckpoints: Reads[Set[Checkpoint]] = Json.reads[Map[String,CheckpointDetails]].map { m =>
+//    m.map {
+//      case (name, details) => Checkpoint(name, details)
+//    }.toSet
+//  }
+
+  def readConfigFrom(configFileObjectId: ObjectId)(implicit objectReader : ObjectReader): Set[Checkpoint] = {
+    val parsedMapResult: JsResult[Map[String, CheckpointDetails]] =
+      Json.fromJson[Map[String, CheckpointDetails]](Json.parse(configFileObjectId.open.getCachedBytes(4096)))
+
+    parsedMapResult.map[Set[Checkpoint]] { m =>
+      m.map {
+        case (name, details) => Checkpoint(name, details)
+      }.toSet
+    }.get
+  }
+
+  case class CheckpointDetails(url: Uri, overdue: Period)
+
+  object Checkpoint {
+    implicit def checkpointToDetails(c: Checkpoint) = c.details
+  }
+
+  case class Checkpoint(name: String, details: CheckpointDetails)
+}
--- app/lib/ConfigFinder.scala
+++ app/lib/ConfigFinder.scala
@@ -1,6 +1,7 @@
 package lib
 
 import com.madgag.git._
+import lib.Config.Checkpoint
 import org.eclipse.jgit.lib.ObjectReader
 import org.eclipse.jgit.revwalk.RevCommit
 import org.eclipse.jgit.treewalk.TreeWalk
@@ -14,4 +15,21 @@ object ConfigFinder {
    * @return treewalk that only returns prout config files
    */
   def configTreeWalk(c: RevCommit)(implicit reader: ObjectReader): TreeWalk = walk(c.getTree)(configFilter)
+
+  def config(c: RevCommit)(implicit reader: ObjectReader): Map[String, Set[Checkpoint]] = {
+    val checkpointsByNameByFolder: Map[String, Set[Checkpoint]] = (for (tw <- configTreeWalk(c)) yield {
+      tw.getPathString -> Config.readConfigFrom(tw.getObjectId(0))
+    }).toMap
+
+    val foldersByCheckpointName: Map[String, Seq[String]] = (for {
+      (folder, checkpointNames) <- checkpointsByNameByFolder.mapValues(_.map(_.name)).toSeq
+      checkpointName <- checkpointNames
+    } yield checkpointName -> folder).groupBy(_._1).mapValues(_.map(_._2))
+
+    val checkpointsNamedInMultipleFolders: Map[String, Seq[String]] = foldersByCheckpointName.filter(_._2.size > 1)
+
+    require(checkpointsNamedInMultipleFolders.isEmpty, s"Duplicate checkpoints defined in multiple config files: $checkpointsNamedInMultipleFolders")
+
+    checkpointsByNameByFolder
+  }
 }
