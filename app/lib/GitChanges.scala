f9d02b529978655b54ce44d3a20c8e631dc4225a
Roberto Tyley <roberto.tyley@gmail.com>
2014-10-14 09:29:28 +0100

    Update GitChanges utility to require slash-prefixed-and-suffixed paths
    eg:
    
    /
    /boo/
    /boo/foo/
    
    not
    
    boo
    boo/foo
    /boo/foo

--- app/lib/GitChanges.scala
+++ app/lib/GitChanges.scala
@@ -4,21 +4,31 @@ import com.madgag.git._
 import org.eclipse.jgit.lib.ObjectReader
 import org.eclipse.jgit.revwalk.RevCommit
 import org.eclipse.jgit.treewalk.TreeWalk
-import org.eclipse.jgit.treewalk.filter.{AndTreeFilter, PathFilterGroup}
+import org.eclipse.jgit.treewalk.filter.{TreeFilter, AndTreeFilter, PathFilterGroup}
 
 import scala.collection.convert.wrapAll._
 
 object GitChanges {
 
-  val treeDiffFilter: TreeWalk => Boolean = w => w.isSubtree && (1 until w.getTreeCount).exists(!w.idEqual(_, 0))
+  val treeDiffFilter: TreeFilter = (w: TreeWalk) => w.isSubtree && (1 until w.getTreeCount).exists(!w.idEqual(_, 0))
 
   /**
    *
    * @return interestingPaths which were affected by the changes base..head
    */
   def affectedFolders(base: RevCommit, head: RevCommit, interestingPaths: Set[String])(implicit reader: ObjectReader): Set[String] = {
-    val treeFilter = AndTreeFilter.create(PathFilterGroup.createFromStrings(interestingPaths), treeDiffFilter)
+    val pathsWhichDoNoStartAndEndWithSlashes = interestingPaths.filterNot(p => p.startsWith("/") && p.endsWith("/"))
+    require(pathsWhichDoNoStartAndEndWithSlashes.isEmpty,
+      s"Interesting paths should start and end with a slash: $pathsWhichDoNoStartAndEndWithSlashes")
 
-    walk(base.getTree, head.getTree)(treeFilter, postOrderTraversal = true).map(_.getPathString).filter(interestingPaths).toSet
+    val (rootPaths, subFolderPaths) = interestingPaths.partition(_ == "/")
+
+    val affectedRootPaths = rootPaths.filter(_ => base.getTree != head.getTree)
+    val affectedSubFolderPaths = if (subFolderPaths.isEmpty) Set.empty else {
+      val treeFilter = AndTreeFilter.create(PathFilterGroup.createFromStrings(subFolderPaths.map(_.stripPrefix("/"))), treeDiffFilter)
+      walk(base.getTree, head.getTree)(treeFilter, postOrderTraversal = true).map(_.slashPrefixedPath + "/").toSet.filter(subFolderPaths)
+    }
+
+    affectedRootPaths ++ affectedSubFolderPaths
   }
 }
--- test/lib/GitChangesSpec.scala
+++ test/lib/GitChangesSpec.scala
@@ -3,28 +3,40 @@ package lib
 import com.madgag.git._
 import lib.GitChanges.affectedFolders
 import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.revwalk.RevCommit
 import org.scalatestplus.play._
 
 class GitChangesSpec extends PlaySpec {
 
   "Multi folder config" must {
 
-    implicit val localGitRepo: Repository = test.unpackRepo("/multi-folder.git.zip")
-
-    "detect changes in affected folders" in {
+    "detect changes in top-level folder" in {
+      implicit val localGitRepo: Repository = test.unpackRepo("/simple.git.zip")
       implicit val (revWalk, reader) = localGitRepo.singleThreadedReaderTuple
 
       def commitAt(revstr: String) = localGitRepo.resolve(revstr).asRevCommit
 
-      val fooBarBaz = Set("foo", "bar", "baz")
+      val modTopLevel: RevCommit = commitAt("mod-top-level")
+      val master: RevCommit = commitAt("master")
 
-      affectedFolders(commitAt("master"), commitAt("mod-foo"), fooBarBaz) mustEqual(Set("foo"))
-      affectedFolders(commitAt("master"), commitAt("mod-foo"), Set("bar")) mustEqual(Set.empty)
+      affectedFolders(master, modTopLevel, Set("/")) mustEqual(Set("/"))
+      affectedFolders(master, modTopLevel, Set("/bar/")) mustEqual(Set.empty)
+    }
 
+    "detect changes in affected folders" in {
+      implicit val localGitRepo: Repository = test.unpackRepo("/multi-folder.git.zip")
+      implicit val (revWalk, reader) = localGitRepo.singleThreadedReaderTuple
 
-      affectedFolders(commitAt("master"), commitAt("mod-foo-and-bar"), fooBarBaz) mustEqual(Set("foo", "bar"))
+      def commitAt(revstr: String) = localGitRepo.resolve(revstr).asRevCommit
 
-      affectedFolders(commitAt("master"), commitAt("del-baz-thing"), fooBarBaz) mustEqual(Set("baz"))
+      val fooBarBaz = Set("/foo/", "/bar/", "/baz/")
+
+      affectedFolders(commitAt("master"), commitAt("mod-foo"), fooBarBaz) mustEqual(Set("/foo/"))
+      affectedFolders(commitAt("master"), commitAt("mod-foo"), Set("/bar/")) mustEqual(Set.empty)
+
+      affectedFolders(commitAt("master"), commitAt("mod-foo-and-bar"), fooBarBaz) mustEqual(Set("/foo/", "/bar/"))
+
+      affectedFolders(commitAt("master"), commitAt("del-baz-thing"), fooBarBaz) mustEqual(Set("/baz/"))
     }
   }
 }
