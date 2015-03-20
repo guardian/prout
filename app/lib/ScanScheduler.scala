78d3c16948c45c909194474b89e616aa0fdd87d6
Roberto Tyley <roberto.tyley@gmail.com>
2015-03-20 21:37:16 +0000

    When scans fail, let's not lose the error to the future

--- app/lib/ScanScheduler.scala
+++ app/lib/ScanScheduler.scala
@@ -12,6 +12,7 @@ import play.api.libs.concurrent.Akka
 import scala.collection.immutable
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 class ScanScheduler(repoFullName: RepoFullName,
                     checkpointSnapshoter: CheckpointSnapshoter,
@@ -23,29 +24,34 @@ class ScanScheduler(repoFullName: RepoFullName,
 
   private val dogpile = new Dogpile(Delayer.delayTheFuture {
     val summariesF = droid.scan(conn.getRepository(repoFullName.text))(checkpointSnapshoter)
-    for (summaries <- summariesF) {
-      Logger.info(s"$selfScanScheduler : ${summaries.size} summaries for ${repoFullName.text}:\n${summaries.map(s => s"#${s.pr.getNumber} ${s.stateChange}").mkString("\n")}")
+    for (summariesTry <- summariesF.trying) {
+      summariesTry match {
+        case Failure(e) =>
+          Logger.error(s"Scanning $repoFullName failed", e)
+        case Success(summaries) =>
+          Logger.info(s"$selfScanScheduler : ${summaries.size} summaries for ${repoFullName.text}:\n${summaries.map(s => s"#${s.pr.getNumber} ${s.stateChange}").mkString("\n")}")
 
-      val scanTimeForPendingOpt = summaries.find(_.hasPendingCheckpoints).map(_ => Instant.now + 1.minute)
+          val scanTimeForPendingOpt = summaries.find(_.hasPendingCheckpoints).map(_ => Instant.now + 1.minute)
 
-      val overdueTimes = summaries.collect {
-        case summary => summary.soonestPendingCheckpointOverdueTime
-      }.flatten
+          val overdueTimes = summaries.collect {
+            case summary => summary.soonestPendingCheckpointOverdueTime
+          }.flatten
 
-      val candidateFollowUpScanTimes = overdueTimes ++ scanTimeForPendingOpt
+          val candidateFollowUpScanTimes = overdueTimes ++ scanTimeForPendingOpt
 
-      if (candidateFollowUpScanTimes.nonEmpty) {
-        val earliestCandidateScanTime: Instant = candidateFollowUpScanTimes.min
-        earliestFollowUpScanTime.send {
-          oldFollowupTime =>
-            val now = DateTime.now
-            if (now > oldFollowupTime || earliestCandidateScanTime < oldFollowupTime) {
-              Akka.system.scheduler.scheduleOnce((now to earliestCandidateScanTime).duration) {
-                scan()
-              }
-              earliestCandidateScanTime
-            } else oldFollowupTime
-        }
+          if (candidateFollowUpScanTimes.nonEmpty) {
+            val earliestCandidateScanTime: Instant = candidateFollowUpScanTimes.min
+            earliestFollowUpScanTime.send {
+              oldFollowupTime =>
+                val now = DateTime.now
+                if (now > oldFollowupTime || earliestCandidateScanTime < oldFollowupTime) {
+                  Akka.system.scheduler.scheduleOnce((now to earliestCandidateScanTime).duration) {
+                    scan()
+                  }
+                  earliestCandidateScanTime
+                } else oldFollowupTime
+            }
+          }
       }
     }
     summariesF
