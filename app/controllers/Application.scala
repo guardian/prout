860dfa0d57cdf0650cb6b5f24c752b22905c472a
Roberto Tyley <roberto.tyley@theguardian.com>
2016-05-10 17:58:48 +0100

    More diagnostics, and whadda you know, my mistake
    https://github.com/rtyley/play-git-hub/commit/3c23fa2288

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -32,13 +32,13 @@ object Application extends Controller {
   def zoomba(repoId: RepoId) = Actions.repoAuthenticated(repoId).async { implicit req =>
     implicit val checkpointSnapshoter = Api.checkpointSnapshoter
     for {
-      RepoWhitelistService.
       wl <- RepoWhitelistService.repoWhitelist.get()
       repoFetchedByProut <- Bot.github.getRepo(repoId)
+      proutPresenceQuickCheck <- RepoWhitelistService.hasProutConfigFile(repoFetchedByProut)
       repoSnapshot <- RepoSnapshot(repoFetchedByProut)
     } yield {
       val known = wl.allKnownRepos(repoId)
-      Ok(views.html.userPages.repo(repoSnapshot))
+      Ok(views.html.userPages.repo(proutPresenceQuickCheck, repoSnapshot))
     }
   }
 
