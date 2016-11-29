9109e7f61da9b57390b940458eabcf8f04eae581
Mario Galic <mario.galic@guardian.co.uk>
2016-11-29 16:33:03 +0000

    Rename TIP to Testing-in-Production

--- app/controllers/TestingInProduction.scala
+++ app/controllers/TestingInProduction.scala
@@ -3,7 +3,7 @@ package controllers
 import lib._
 import play.api.mvc._
 
-object Tip extends Controller {
+object TestingInProduction extends Controller {
 
   def testFeedback() = Action.async(parse.json) { implicit request =>
     TestFeedback.notifyGitHub(request.body.as[TestResult])
