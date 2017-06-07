f09e73eac0825e4a2975cf563bb828466685bfc3
Thomas Bonnin <thomas.bonnin@gmail.com>
2017-06-07 16:44:10 +0100

    Add checkpoint custom message feature
    {
      "url": "https://www.theguardian.com",
      "overdue": "20M",
      "messages": {
        "seen": "prout/seen.md",
        "overdue": "prout/overdue.md"
      }
    }
    The value for each status would be the path of a file contained in the
    repository
    The file content would be appended to the comment if set.
    If no custom message is set, the default string 'Please check your changes!' would be appended

---
+++ app/lib/FileFinder.scala
@@ -0,0 +1,24 @@
+package lib
+
+import com.madgag.git._
+import org.eclipse.jgit.lib.ObjectId
+import org.eclipse.jgit.revwalk.RevCommit
+import org.eclipse.jgit.treewalk.TreeWalk
+import scala.util.Try
+
+class FileFinder(commit: RevCommit)(implicit repoThreadLocal: ThreadLocalObjectDatabaseResources) {
+
+  implicit val reader = repoThreadLocal.reader()
+
+  private[lib] def objectIdForPath(path: String): Option[ObjectId] =
+    Try(
+      TreeWalk
+        .forPath(reader, path, commit.getTree)
+        .getObjectId(0)
+    ).toOption
+
+  def read(path: String): Option[String] = {
+    objectIdForPath(path).map(_.open.getCachedBytes(4096).toString)
+  }
+}
+
