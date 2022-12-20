35e50f16682b910b063466b566b39f4fc9b4a032
Ioanna Kokkini <ioannakok@hotmail.com>
2022-12-20 15:34:53 +0000

    Prout is checking for commit id in http response headers
    We want to be able to read commit ids from frontend which does not always have control over the html, so we want to use response headers instead.
    
    Co-authored-by: Roberto Tyley <roberto.tyley@guardian.co.uk>
    Co-authored-by: Georges Lebreton <georges.lebreton@guardian.co.uk>
    Co-authored-by: Daniel Clifton <daniel.clifton@guardian.co.uk>

--- test/lib/CheckpointSpec.scala
+++ test/lib/CheckpointSpec.scala
@@ -15,5 +15,17 @@ class CheckpointSpec extends PlaySpec with ScalaFutures with Eventually with Int
         Checkpoint("PROD", CheckpointDetails("https://dashboard.ophan.co.uk/login", Period.parse("PT1H")))
       whenReady(CheckpointSnapshoter.snapshot(checkpoint)) { _ must not be empty }
     }
+
+    // TODO: We add this test once this gets merged: https://github.com/guardian/frontend/pull/25795
+//    "be able to hit the UK network front" in {
+//      val checkpoint =
+//        Checkpoint("CODE", CheckpointDetails("https://m.code.dev-theguardian.com/uk", Period.parse("PT1H")))
+//      whenReady(CheckpointSnapshoter.snapshot(checkpoint)) { gitCommitIds =>
+//        val commitIds = gitCommitIds.toList
+//        println("Git commit ids:" + commitIds)
+//        println(commitIds.length)
+//        commitIds must not be empty
+//      }
+//    }
   }
 }
