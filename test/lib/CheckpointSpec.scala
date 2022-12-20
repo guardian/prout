be6afeba62d2c37f1c88a684ebff5c7692641a6b
Ioanna Kokkini <ioannakok@hotmail.com>
2022-12-20 16:43:12 +0000

    Add unit test to check that checkpoint snapshots can hit UK network front
    Changes introduced in these PRs:
    https://github.com/guardian/prout/pull/79
    https://github.com/guardian/frontend/pull/25795
    
    Co-authored-by: Roberto Tyley <roberto.tyley@guardian.co.uk>
    Co-authored-by: Georges Lebreton <georges.lebreton@guardian.co.uk>
    Co-authored-by: Daniel Clifton <daniel.clifton@guardian.co.uk>

--- test/lib/CheckpointSpec.scala
+++ test/lib/CheckpointSpec.scala
@@ -16,16 +16,13 @@ class CheckpointSpec extends PlaySpec with ScalaFutures with Eventually with Int
       whenReady(CheckpointSnapshoter.snapshot(checkpoint)) { _ must not be empty }
     }
 
-    // TODO: We add this test once this gets merged: https://github.com/guardian/frontend/pull/25795
-//    "be able to hit the UK network front" in {
-//      val checkpoint =
-//        Checkpoint("CODE", CheckpointDetails("https://m.code.dev-theguardian.com/uk", Period.parse("PT1H")))
-//      whenReady(CheckpointSnapshoter.snapshot(checkpoint)) { gitCommitIds =>
-//        val commitIds = gitCommitIds.toList
-//        println("Git commit ids:" + commitIds)
-//        println(commitIds.length)
-//        commitIds must not be empty
-//      }
-//    }
+    "be able to hit the UK network front" in {
+      val checkpoint =
+        Checkpoint("PROD", CheckpointDetails("https://www.theguardian.com/uk", Period.parse("PT1H")))
+      whenReady(CheckpointSnapshoter.snapshot(checkpoint)) { gitCommitIds =>
+        val commitIds = gitCommitIds.toList
+        commitIds must not be empty
+      }
+    }
   }
 }
